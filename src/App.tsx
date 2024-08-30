a73194b58be564e791cb0cba729bf9e1b6ec226b
AtharvT <atharv.tare.03@gmail.com>
2024-08-31 02:07:24 +0530

    feat!: setup hero section with dummy navbar items

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { FaHome } from "react-icons/fa";
+import Hero from "./components/Hero";
+
+import { ThemeProvider } from "./components/theme-provider";
+import { FloatingNav } from "./components/ui/floating-navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Soon to be portfolio...</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+        <div className="max-w-7xl w-full">
+          <FloatingNav
+            navItems={[
+              { name: "Home", link: "#home", icon: <FaHome /> },
+              { name: "About", link: "#about",  },
+              { name: "Projects", link: "#projects" },
+              { name: "Contact", link: "#contact" },
+            ]}
+          />
+          <Hero />
+        </div>
+      </main>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
